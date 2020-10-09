9e516e2e28379b777fff2f21abd5b42ebfeefbbc
Filip Cvetko <filipcvetk123@gmail.com>
2020-10-09 14:43:11 +0200

    Scraper.py modified based on OOP style, appdata.json was added to acount for user preferences. Each offer now test, whether the same offer has already been included in th csv file. If the offer id is present in appdata.json["visited"] then writing the offer will be skipped. Next task is to use regex to find two variables from description -> year built and year renovated.

--- scraper.py
+++ scraper.py
@@ -1,49 +1,85 @@
 # This script will scrape the entire nepremicnine.net rental apartments for Ljubljana city.
 
-from bs4 import BeautifulSoup
 from lxml import html
 import requests
 import csv
-import sys
+import ujson
+import re
 
-base_URL = sys.argv[1]
+class Scraper:
 
-def check_total_pages(base_url):
-    r = requests.get(base_url)
-    page_tree = html.fromstring(r.content)
-    total_pages = int(page_tree.xpath('//div[@id="pagination"]/ul/@data-pages')[0])
-    return total_pages
+    def __init__(self, appdata_file):
+        with open(appdata_file, "r") as file:
+            appdata = ujson.load(file)
+        if not appdata:
+            raise FileNotFoundError(f"Could not load {appdata_file}.")
+        self._appdata = appdata
+        self._appdata_file = appdata_file
+        self._baseURL = appdata["baseURL"]
+        
 
-def check_all_pages(base_url):
-    num_pages = check_total_pages(base_url)
-    num_of_items = 0
+    def does_offer_exist(self, offer_id):
+        visited = self._appdata["visited"]
+        for item in visited:
+            if str(item) == str(offer_id):
+                return True
+        return False 
 
-    csv_first_line = ["title","offer_type","desc","size","price"]
-
-    with open("generated_file.csv", "w") as file:
-        writer = csv.writer(file, delimiter="|")
-        writer.writerow(csv_first_line)
-
-    for page in range(num_pages):
-        current_page_number = page + 1
-        current_page_URL = base_URL + f"{current_page_number}/"
-        print(current_page_URL)
-        r = requests.get(current_page_URL)
+    def check_total_pages(self):
+        r = requests.get(self._baseURL)
         page_tree = html.fromstring(r.content)
-        offers = page_tree.xpath('//div[@class="seznam"]/div[@itemprop="itemListElement"]')
+        total_pages = int(page_tree.xpath('//div[@id="pagination"]/ul/@data-pages')[0])
+        return total_pages
 
-        for offer in offers:
+    def check_all_pages(self):
+        num_pages = self.check_total_pages()
+        num_of_items = 0
 
-            title = offer.xpath("div/h2/a/span/text()")[0]
-            offer_type = offer.xpath('div/div/span/span[@class="tipi"]/text()')[0]
-            desc = offer.xpath('div/div/div[@class="kratek_container"]/div/text()')[0]
-            size = offer.xpath('div/div/div[@class="main-data"]/span[@class="velikost"]/text()')[0]
-            price = offer.xpath('div/div/div[@class="main-data"]/span[@class="cena"]/text()')[0]
+        csv_first_line = ["title","offer_type","desc","size","price"]
 
-            item_components = [title,offer_type,desc,size,price]
+        with open("generated_file.csv", "w") as file:
+            writer = csv.writer(file, delimiter="|")
+            writer.writerow(csv_first_line)
 
-            with open("generated_file.csv", "a") as file:
-                writer = csv.writer(file, delimiter="|")
-                writer.writerow(item_components)
+        for page in range(num_pages):
+            current_page_number = page + 1
+            current_page_URL = self._baseURL + f"{current_page_number}/"
+            print(current_page_URL)
+            r = requests.get(current_page_URL)
+            page_tree = html.fromstring(r.content)
+            offers = page_tree.xpath('//div[@class="seznam"]/div[@itemprop="itemListElement"]')
+
+            for offer in offers:
+
+                title = offer.xpath("div/h2/a/span/text()")[0]
+                offer_type = offer.xpath('div/div/span/span[@class="tipi"]/text()')[0]
+                desc = offer.xpath('div/div/div[@class="kratek_container"]/div/text()')[0]
+                size = offer.xpath('div/div/div[@class="main-data"]/span[@class="velikost"]/text()')[0]
+                price = offer.xpath('div/div/div[@class="main-data"]/span[@class="cena"]/text()')[0]
+                offer_id = offer.xpath('attribute::id')[0]
+
+                # Uporabimo reg-ex, da najdemo leto gradnje in adaptacije.
+                gradnja = re.search("")
+
+                item_components = [title,offer_type,desc,size,price]
+
+                if self.does_offer_exist(offer_id):
+                    continue
+                
+                # In case the offer does not exist
+                # Append the information to the csv file and append the offer id to visited list.
+                with open("generated_file.csv", "a") as file:
+                    writer = csv.writer(file, delimiter="|")
+                    writer.writerow(item_components)
+
+                self._appdata["visited"].append(offer_id)
+
+        # Now we have to write self._appdata, which is stored locally in the script, to the
+        # appdata.json in the directory.
+        with open(self._appdata_file, "w") as file:
+            ujson.dump(self._appdata, file, indent=2)
             
-check_all_pages(base_URL)
+APPDATA_FILE = "appdata.json"
+
+scraper = Scraper(APPDATA_FILE)
+scraper.check_all_pages()
